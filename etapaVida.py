e5077dffb4f050e6fbb59f072f2d761c1e107d06
mari sanchez <marilirey1122@gmail.com>
2022-03-04 11:32:22 -0500

    EVE422 se crea ejercicio de 4 funciones para imprimir en consola la etapa de la vida segun la edad

---
+++ etapaVida.py
@@ -0,0 +1,20 @@
+#etapa de la vida, según edad(EVE422)
+
+etapaVida= int(input("digite la etapa de la vida, según la edad: "))
+if(etapaVida >=0 and etapaVida <14):
+    print("Niño")
+elif(etapaVida >=15 and etapaVida <28):
+    print("Joven")
+elif(etapaVida >=28 and etapaVida <60):
+    print("Adulto") 
+else:
+    print("Adulto mayor")     
+
+
+
+
+
+
+
+
+  
